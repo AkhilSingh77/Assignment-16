a66859909e2ba7c65c38538da80516c1d05d086c
AkhilSingh77 <akhil.singh@axeno.co>
2024-08-05 21:48:22 +0530

    Setting up the ui

---
+++ Task2/script.js
@@ -0,0 +1,3 @@
+const countries = ["United States","Canada","Mexico","Brazil","India","Iran","China","Japan","Germany","France","United Kingdom","Italy","Australia","Russia","South Korea","Spain","Turkey","Netherlands","Saudi Arabia","Switzerland","Argentina","South Africa","Nigeria","Egypt","Sweden","Norway","Denmark","Finland","New Zealand","Singapore"];
+
+  
