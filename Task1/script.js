bf7d678423c4d57b698c413b169c0d5e3ae9bfb1
AkhilSingh77 <akhil.singh@axeno.co>
2024-08-05 18:06:09 +0530

    all functionalities done ui remaining

--- Task1/script.js
+++ Task1/script.js
@@ -14,13 +14,23 @@ let allTags =[];
 const tagContainer = document.getElementsByClassName("tagcontainer")[0];
 const input  = document.getElementsByClassName("wrapper--text")[0];
 
+function removetag(element ,tag){
+    let index = allTags.indexOf(tag);
+
+    // console.log('hii');
+    // console.log(element,tag);
+    // console.log(index);
+    allTags.splice(index,1);
+    // console.log("new",allTags)
+    element.parentElement.remove();
+}
 
 function createTag(){
     tagContainer.querySelectorAll(".sitag").forEach(tag => tag.remove())
     allTags.forEach((tag)=>{
         let singleTag = `<div class="sitag">
           <p>${tag}</p>
-          <i class="fa-regular fa-circle-xmark"></i>
+          <i class="fa-regular fa-circle-xmark" onclick ="removetag(this ,'${tag}')" ></i>
         </div>`
         tagContainer.insertAdjacentHTML("afterbegin",singleTag);
 
